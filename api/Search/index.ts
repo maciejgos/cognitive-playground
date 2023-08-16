4a39736fb68209ccd428ba5e0825d1917556c970
Maciej Gos <maciej@maciejgos.com>
2023-08-16 14:44:49 +0000

    Implement first API endpoint

--- api/Search/index.ts
+++ api/Search/index.ts
@@ -2,10 +2,35 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
+    const results = [
+        {
+          hotel: "Hotel 1",
+          price: 100,
+        },
+        {
+          hotel: "Hotel 2",
+          price: 200,
+        },
+        {
+          hotel: "Hotel 3",
+          price: 300,
+        },
+        {
+          hotel: "Hotel 4",
+          price: 400,
+        },
+        {
+          hotel: "Hotel 5",
+          price: 500,
+        },
+        {
+          hotel: "Hotel 6",
+          price: 600,
+        }
+      ];
+
     const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const responseMessage = results;
 
     context.res = {
         // status: 200, /* Defaults to 200 */
--- web/app/components/SearchResultsComponent.tsx
+++ web/app/components/SearchResultsComponent.tsx
@@ -1,7 +1,27 @@
 /*
+  Results search object structure it should have field with a name of hotel and price
+*/
+
+interface SearchResult {
+  hotel: string;
+  price: number;
+}
+
+/*
  Component to display search results in the center of UI
 */
-export function SearchResultsComponent() {
+import React from 'react';
+
+interface SearchResult {
+  hotel: string;
+  price: number;
+}
+
+interface Props {
+  results: SearchResult[];
+}
+
+export function SearchResultsComponent({ results }: Props) {
   return (
     <div className="container mx-auto px-4 object-right">
       <div className="flex flex-col">
@@ -11,42 +31,14 @@ export function SearchResultsComponent() {
           </span>
           <span>Sort By</span>
         </div>
-        <div className="flex flex-row justify-between items-center p-4">
-          <span className="font-semibold text-xl tracking-tight">
-            Hotel Name
-          </span>
-          <span>Price</span>
-        </div>
-        <div className="flex flex-row justify-between items-center p-4">
-          <span className="font-semibold text-xl tracking-tight">
-            Hotel Name
-          </span>
-          <span>Price</span>
-        </div>
-        <div className="flex flex-row justify-between items-center p-4">
-          <span className="font-semibold text-xl tracking-tight">
-            Hotel Name
-          </span>
-          <span>Price</span>
-        </div>
-        <div className="flex flex-row justify-between items-center p-4">
-          <span className="font-semibold text-xl tracking-tight">
-            Hotel Name
-          </span>
-          <span>Price</span>
-        </div>
-        <div className="flex flex-row justify-between items-center p-4">
-          <span className="font-semibold text-xl tracking-tight">
-            Hotel Name
-          </span>
-          <span>Price</span>
-        </div>
-        <div className="flex flex-row justify-between items-center p-4">
-          <span className="font-semibold text-xl tracking-tight">
-            Hotel Name
-          </span>
-          <span>Price</span>
-        </div>
+        {results.map((result, index) => (
+          <div className="flex flex-row justify-between items-center p-4" key={index}>
+            <span className="font-semibold text-xl tracking-tight">
+              {result.hotel}
+            </span>
+            <span>{result.price}</span>
+          </div>
+        ))}
       </div>
     </div>
   );
